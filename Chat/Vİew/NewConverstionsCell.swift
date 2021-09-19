2c152ff1c6b0770c3ea79ede46945c8c355e5bf5
Samet Buğra Oktay <40534807+sametbugraoktay@users.noreply.github.com>
2021-09-19 11:23:17 +0300

    Delete NewConverstionsCell.swift

--- Chat/Vİew/NewConverstionsCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NewConverstionsCell.swift
-//  Chat
-//
-//  Created by Samet Bugra Oktay on 18.08.2020.
-//  Copyright © 2020 Samet Bugra Oktay. All rights reserved.
-//
-
-import Foundation
